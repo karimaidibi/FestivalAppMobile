f1531f6e6918d7442a649a52356e4334e6cf957c
karim aidibi <karim.aidibi@etu.umontpellier.fr>
2023-03-24 14:07:00 +0100

    Initial Commit

---
+++ FestivalAppMobile/FestivalAppMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestivalAppMobileApp.swift
+//  FestivalAppMobile
+//
+//  Created by m1 on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FestivalAppMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
